353dfe83e6d6fa9909012abf8e5208ba7517e4ed
Ruben Leon <productiveidiot@gmail.com>
2025-10-30 12:32:25 -0700

    Finished main functionality of extracting img path and displaying image

--- rf_manager.sh
+++ rf_manager.sh
@@ -5,8 +5,8 @@ IMAGE_PATH=$1
 NUM_IMAGES=$2
 MINUTES_PER_IMAGE=$3
 
-if [ $# -le 3 ]; then 
-    echo "Usage: $0 [OPTION] <IMAGE_PATH> <NUM_IMAGES> <MINUTES_PER_IMAGE>"
+if [ $# -lt 3 ]; then 
+    echo "Usage: $0 <IMAGE_PATH> <NUM_IMAGES> <MINUTES_PER_IMAGE>"
     exit 1 
 fi
 
@@ -17,32 +17,38 @@ if [ ! -d "$IMAGE_PATH" ]; then
 fi
 
 # Check that NUM_IMAGE and MINUTES_PER_IMAGE is a numerical value 
-if [[ ! "$NUM_IMAGES" ~= ^[0-9]+ ]]; then 
+if [[ ! "$NUM_IMAGES" =~ ^[0-9]+$ ]]; then 
     echo "$NUM_IMAGES is not a numerical value."
     exit 1 
 fi
 
-if [[ ! "$MINUTES_PER_IMAGE" ~= ^[0-9]+ ]]; then 
+if [[ ! "$MINUTES_PER_IMAGE" =~ ^[0-9]+$ ]]; then 
     echo "$MINUTES_PER_IMAGE is not a numerical value."
     exit 1 
 fi
 
 # Finding the image paths and redirecting output to txt file 
-find "$IMAGE_PATH" -iregex ".*\.\(jpg\|jpeg\|png\|gif\|bmp\|svg\)" | shuf | head --lines="$NUM_IMAGES" > "image_paths.txt"
+find "$IMAGE_PATH" -iregex "^.*\.\(jpg\|jpeg\|png\|gif\|bmp\|svg\)$" | shuf | head --lines="$NUM_IMAGES" > "IMAGE_PATHS.txt"
 
 # Looping through the image paths and displaying the reference
-MAX_IMAGES=$(wc -l "image_paths.txt")
+IMG_FILE="IMAGE_PATHS.txt"
+MAX_IMAGES=$(wc -l "IMAGE_PATHS.txt" | awk '{print $1}')
 CURR_IMAGE=1
 
-while [ "$CURR_IMAGE" -ne "$MAX_IMAGES" ]; do
+# While the current image is less than or equal to 
+while [ $((CURR_IMAGE)) -le $((MAX_IMAGES)) ]; do
+    echo "am in LOOP"
     # Find the current image path
-    CURR_IMAGE_PATH=$(head -n $CURR_IMAGE)
+    CURR_IMAGE_PATH=$(sed -n "${CURR_IMAGE}p" $IMG_FILE)
 
     # Executing the image viewer 
-    feh --scale-down $CURR_IMAGE_PATH &
+    feh --scale-down "$CURR_IMAGE_PATH" &
     FEH_PID=$!
-    sleep "${MINUTES_PER_IMAGE}m"
+    sleep "${MINUTES_PER_IMAGE}s"
+
+    # Killing the image viewer and updating the CURR_IMAGE
     kill $FEH_PID
+    CURR_IMAGE=$((CURR_IMAGE+1))
 done 
 
 echo "Finished with session!"
